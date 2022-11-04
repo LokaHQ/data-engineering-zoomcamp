e3a6e356720fafa6adfd5a1d4b3f43a1de8a4f64
toshevnikola <toshevnikola@outlook.com>
2022-11-04 16:08:39 +0100

    W2 homework

---
+++ week_2_data_ingestion/homework/dags/yellow_taxi_ingestion_dag.py
@@ -0,0 +1,91 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from google.cloud import storage
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateExternalTableOperator,
+)
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+dataset_name = "yellow_tripdata_{{ execution_date.strftime('%Y-%m') }}"
+dataset_file = f"{dataset_name}.parquet"
+dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
+
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "trips_data_all")
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+with DAG(
+    dag_id="yellow_taxi_ingestion_dag",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(year=2019, month=1, day=1),
+    end_date=datetime(year=2021, month=1, day=1),
+    default_args=default_args,
+    max_active_runs=1,
+    tags=["dtc-de"],
+) as dag:
+
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}",
+    )
+
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{dataset_file}",
+            "local_file": f"{path_to_local_home}/{dataset_file}",
+        },
+    )
+
+    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": dataset_name,
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/raw/{dataset_file}"],
+            },
+        },
+    )
+
+    download_dataset_task >> local_to_gcs_task  # >> bigquery_external_table_task
