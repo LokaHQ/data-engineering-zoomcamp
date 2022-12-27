2f74de4592c7d828f15f87e389363fba842b63ad
carlosloka <carlos@loka.com>
2022-12-27 14:09:38 +0000

    update docker-compose

--- week_6_stream_processing/consumer.py
+++ week_6_stream_processing/consumer.py
@@ -9,11 +9,11 @@ consumer = KafkaConsumer(
      enable_auto_commit=True,
      group_id='consumer.group.id.demo.2',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
+   
 
-
-while(True):
- print("inside while")
- for message in consumer:
-  message = message.value
-  print(message)
- sleep(1)
+while (True):
+    print("inside while")
+    for message in consumer:
+        message = message.value
+        print(message)
+        sleep(1)
