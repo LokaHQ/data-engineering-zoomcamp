99e9dbd9cdf09b55af6eb91ca905e71ebb906e25
Stefan Kochev <stefan_kochev@yahoo.com>
2023-01-21 15:52:50 +0100

    Project - Ingestion BigQuery

--- project/airflow/dags/ingestion_dag.py
+++ project/airflow/dags/ingestion_dag.py
@@ -6,6 +6,11 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.models.param import Param
 
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateExternalTableOperator,
+    BigQueryInsertJobOperator,
+)
+
 from ingestion_dag_functions import ingest_gcs_callable, commit_file, format_to_parquet
 
 
@@ -15,6 +20,8 @@ BUCKET = os.environ.get("GCP_GCS_BUCKET")
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "airlines_data_all")
 
 
+# Sample input parameters payload:
+# {"csv_file_download_url":"https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/IXITH2","year":"1987"}
 with DAG(
     "IngestionDAG",
     schedule=None,
@@ -25,7 +32,6 @@ with DAG(
         "csv_file_download_url": Param(default="", type="string"),
         "year": Param(default="", type="string"),  # Example 1998
     },
-    # {"csv_file_download_url":"https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/IXITH2","year":"1987"}
 ) as dag:
 
     wget_task = BashOperator(
@@ -72,6 +78,39 @@ with DAG(
         ),
     )
 
+    big_query_external_table_task = BigQueryCreateExternalTableOperator(
+        task_id="bq-create-external-table",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": "airlines_external_table",
+            },
+            "externalDataConfiguration": {
+                "autodetect": "True",
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/raw/parquet/*"],
+            },
+        },
+    )
+
+    BQ_CREATE_TABLE_QUERY = (
+        f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.airlines_data "
+        f"AS "
+        f"SELECT * FROM {BIGQUERY_DATASET}.airlines_external_table;"
+    )
+
+    # Create/Refresh a partitioned table from external table
+    big_query_create_table_task = BigQueryInsertJobOperator(
+        task_id=f"bq-create-table",
+        configuration={
+            "query": {
+                "query": BQ_CREATE_TABLE_QUERY,
+                "useLegacySql": False,
+            }
+        },
+    )
+
     cleanup_task = BashOperator(
         task_id="cleanup",
         bash_command="rm -f $airflow_home/airlines_{{params.year}}.csv $airflow_home/airlines_{{params.year}}.parquet",
@@ -92,6 +131,8 @@ with DAG(
         >> bzip_task
         >> format_to_parquet_task
         >> [upload_task_csv, upload_task_parquet]
+        >> big_query_external_table_task
+        >> big_query_create_table_task
         >> cleanup_task
         >> commit_file_task
     )
