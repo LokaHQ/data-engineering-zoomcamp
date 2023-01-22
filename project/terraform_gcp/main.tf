29aaa265eaab4505f80bbff9cae626da1797677c
Stefan Kochev <stefan_kochev@yahoo.com>
2023-01-22 18:43:39 +0100

    Project - Terraform add depends_on

--- project/terraform_gcp/main.tf
+++ project/terraform_gcp/main.tf
@@ -43,12 +43,16 @@ resource "google_storage_bucket_object" "source_file" {
   name   = "metadata/source_files.csv"
   source = "datalake_metadata/source_files_sample.csv"
   bucket = "${local.data_lake_bucket}_${var.project}"
+
+  depends_on = [google_storage_bucket.data-lake-bucket]
 }
 
 resource "google_storage_bucket_object" "source_file_commit_log" {
   name   = "metadata/source_files_commit_log.csv"
   source = "datalake_metadata/source_files_commit_log_sample.csv"
   bucket = "${local.data_lake_bucket}_${var.project}"
+
+  depends_on = [google_storage_bucket.data-lake-bucket]
 }
 
 # Data Warehouse (BigQuery Dataset)
